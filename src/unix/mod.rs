0b7e1605e6c4d6b80e5189ddde7928af1d9bbe35
Jack O'Connor <oconnor663@gmail.com>
2016-08-20 00:32:28 -0400

    implement the dup functions for unix

--- src/unix/mod.rs
+++ src/unix/mod.rs
@@ -4,8 +4,10 @@ mod cvt;
 mod fd;
 mod pipe;
 
+use libc;
 use std::fs::File;
 use std::io;
+use std::mem;
 use std::process::Stdio;
 use std::os::unix::io::{FromRawFd, IntoRawFd};
 
@@ -25,14 +27,23 @@ pub fn pipe() -> io::Result<Pair> {
     }
 }
 
+fn dup_fd(fd: libc::c_int) -> io::Result<Stdio> {
+    let temp = unsafe { File::from_raw_fd(fd) };
+    // Note that we don't return early if this duplicate fails. We *must*
+    // forget the temp File, to avoid closing the original handle.
+    let dup_result = temp.try_clone();
+    mem::forget(temp);
+    dup_result.map(stdio_from_file)
+}
+
 pub fn dup_stdin() -> io::Result<Stdio> {
-    unimplemented!()
+    dup_fd(libc::STDIN_FILENO)
 }
 
 pub fn dup_stdout() -> io::Result<Stdio> {
-    unimplemented!()
+    dup_fd(libc::STDOUT_FILENO)
 }
 
 pub fn dup_stderr() -> io::Result<Stdio> {
-    unimplemented!()
+    dup_fd(libc::STDERR_FILENO)
 }
