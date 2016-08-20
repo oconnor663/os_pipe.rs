8a231ee853dca122b5331fbd3366c3e55fe0060f
Jack O'Connor <oconnor663@gmail.com>
2016-08-19 22:52:31 -0400

    delete functions we don't need

--- src/unix/fd.rs
+++ src/unix/fd.rs
@@ -8,10 +8,9 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-use std::io::{self, Read};
-use libc::{self, c_int, size_t, c_void};
+use std::io;
+use libc::{self, c_int};
 use std::mem;
-use std::sync::atomic::{AtomicBool, Ordering};
 use sys::cvt::cvt;
 
 pub struct FileDesc {
@@ -34,27 +33,6 @@ impl FileDesc {
         fd
     }
 
-    pub fn read(&self, buf: &mut [u8]) -> io::Result<usize> {
-        let ret = try!(cvt(unsafe {
-            libc::read(self.fd,
-                       buf.as_mut_ptr() as *mut c_void,
-                       buf.len() as size_t)
-        }));
-        Ok(ret as usize)
-    }
-
-    pub fn read_to_end(&self, buf: &mut Vec<u8>) -> io::Result<usize> {
-        let mut me = self;
-        (&mut me).read_to_end(buf)
-    }
-
-    pub fn write(&self, buf: &[u8]) -> io::Result<usize> {
-        let ret = try!(cvt(unsafe {
-            libc::write(self.fd, buf.as_ptr() as *const c_void, buf.len() as size_t)
-        }));
-        Ok(ret as usize)
-    }
-
     #[cfg(not(any(target_env = "newlib", target_os = "solaris", target_os = "emscripten")))]
     pub fn set_cloexec(&self) -> io::Result<()> {
         unsafe {
@@ -69,70 +47,6 @@ impl FileDesc {
             try!(cvt(libc::fcntl(self.fd, libc::F_SETFD, previous | libc::FD_CLOEXEC)));
             Ok(())
         }
-    }
-
-    pub fn set_nonblocking(&self, nonblocking: bool) -> io::Result<()> {
-        unsafe {
-            let previous = try!(cvt(libc::fcntl(self.fd, libc::F_GETFL)));
-            let new = if nonblocking {
-                previous | libc::O_NONBLOCK
-            } else {
-                previous & !libc::O_NONBLOCK
-            };
-            try!(cvt(libc::fcntl(self.fd, libc::F_SETFL, new)));
-            Ok(())
-        }
-    }
-
-    pub fn duplicate(&self) -> io::Result<FileDesc> {
-        // We want to atomically duplicate this file descriptor and set the
-        // CLOEXEC flag, and currently that's done via F_DUPFD_CLOEXEC. This
-        // flag, however, isn't supported on older Linux kernels (earlier than
-        // 2.6.24).
-        //
-        // To detect this and ensure that CLOEXEC is still set, we
-        // follow a strategy similar to musl [1] where if passing
-        // F_DUPFD_CLOEXEC causes `fcntl` to return EINVAL it means it's not
-        // supported (the third parameter, 0, is always valid), so we stop
-        // trying that.
-        //
-        // Also note that Android doesn't have F_DUPFD_CLOEXEC, but get it to
-        // resolve so we at least compile this.
-        //
-        // [1]: http://comments.gmane.org/gmane.linux.lib.musl.general/2963
-        #[cfg(target_os = "android")]
-        use libc::F_DUPFD as F_DUPFD_CLOEXEC;
-        #[cfg(not(target_os = "android"))]
-        use libc::F_DUPFD_CLOEXEC;
-
-        let make_filedesc = |fd| {
-            let fd = FileDesc::new(fd);
-            try!(fd.set_cloexec());
-            Ok(fd)
-        };
-        lazy_static! {
-            static ref TRY_CLOEXEC: AtomicBool = AtomicBool::new(!cfg!(target_os = "android"));
-        }
-        let fd = self.raw();
-        if TRY_CLOEXEC.load(Ordering::Relaxed) {
-            match cvt(unsafe { libc::fcntl(fd, F_DUPFD_CLOEXEC, 0) }) {
-                // We *still* call the `set_cloexec` method as apparently some
-                // linux kernel at some point stopped setting CLOEXEC even
-                // though it reported doing so on F_DUPFD_CLOEXEC.
-                Ok(fd) => {
-                    return Ok(if cfg!(target_os = "linux") {
-                        try!(make_filedesc(fd))
-                    } else {
-                        FileDesc::new(fd)
-                    })
-                }
-                Err(ref e) if e.raw_os_error() == Some(libc::EINVAL) => {
-                    TRY_CLOEXEC.store(false, Ordering::Relaxed);
-                }
-                Err(e) => return Err(e),
-            }
-        }
-        cvt(unsafe { libc::fcntl(fd, libc::F_DUPFD, 0) }).and_then(make_filedesc)
     }
 }
 
--- src/unix/pipe.rs
+++ src/unix/pipe.rs
@@ -8,11 +8,8 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-use std::cmp;
 use std::io;
 use libc::{self, c_int};
-use std::mem;
-use std::ptr;
 use sys::cvt::cvt_r;
 use sys::fd::FileDesc;
 
@@ -30,8 +27,8 @@ pub fn anon_pipe() -> io::Result<(AnonPipe, AnonPipe)> {
     // 2.6.27, however, and because we support 2.6.18 we must detect this
     // support dynamically.
     if cfg!(target_os = "linux") {
-        weak! { fn pipe2(*mut c_int, c_int) -> c_int }
-        if let Some(pipe) = pipe2.get() {
+        weak! { fn PIPE2(*mut c_int, c_int) -> c_int }
+        if let Some(pipe) = PIPE2.get() {
             match cvt_r(|| unsafe { pipe(fds.as_mut_ptr(), libc::O_CLOEXEC) }) {
                 Ok(_) => {
                     return Ok((AnonPipe(FileDesc::new(fds[0])), AnonPipe(FileDesc::new(fds[1]))))
@@ -56,73 +53,7 @@ impl AnonPipe {
         Ok(AnonPipe(fd))
     }
 
-    pub fn read(&self, buf: &mut [u8]) -> io::Result<usize> {
-        self.0.read(buf)
-    }
-
-    pub fn read_to_end(&self, buf: &mut Vec<u8>) -> io::Result<usize> {
-        self.0.read_to_end(buf)
-    }
-
-    pub fn write(&self, buf: &[u8]) -> io::Result<usize> {
-        self.0.write(buf)
-    }
-
-    pub fn fd(&self) -> &FileDesc {
-        &self.0
-    }
     pub fn into_fd(self) -> FileDesc {
         self.0
-    }
-}
-
-pub fn read2(p1: AnonPipe, v1: &mut Vec<u8>, p2: AnonPipe, v2: &mut Vec<u8>) -> io::Result<()> {
-    // Set both pipes into nonblocking mode as we're gonna be reading from both
-    // in the `select` loop below, and we wouldn't want one to block the other!
-    let p1 = p1.into_fd();
-    let p2 = p2.into_fd();
-    try!(p1.set_nonblocking(true));
-    try!(p2.set_nonblocking(true));
-
-    let max = cmp::max(p1.raw(), p2.raw());
-    loop {
-        // wait for either pipe to become readable using `select`
-        try!(cvt_r(|| unsafe {
-            let mut read: libc::fd_set = mem::zeroed();
-            libc::FD_SET(p1.raw(), &mut read);
-            libc::FD_SET(p2.raw(), &mut read);
-            libc::select(max + 1,
-                         &mut read,
-                         ptr::null_mut(),
-                         ptr::null_mut(),
-                         ptr::null_mut())
-        }));
-
-        // Read as much as we can from each pipe, ignoring EWOULDBLOCK or
-        // EAGAIN. If we hit EOF, then this will happen because the underlying
-        // reader will return Ok(0), in which case we'll see `Ok` ourselves. In
-        // this case we flip the other fd back into blocking mode and read
-        // whatever's leftover on that file descriptor.
-        let read = |fd: &FileDesc, dst: &mut Vec<u8>| {
-            match fd.read_to_end(dst) {
-                Ok(_) => Ok(true),
-                Err(e) => {
-                    if e.raw_os_error() == Some(libc::EWOULDBLOCK) ||
-                       e.raw_os_error() == Some(libc::EAGAIN) {
-                        Ok(false)
-                    } else {
-                        Err(e)
-                    }
-                }
-            }
-        };
-        if try!(read(&p1, v1)) {
-            try!(p2.set_nonblocking(false));
-            return p2.read_to_end(v2).map(|_| ());
-        }
-        if try!(read(&p2, v2)) {
-            try!(p1.set_nonblocking(false));
-            return p1.read_to_end(v1).map(|_| ());
-        }
     }
 }
